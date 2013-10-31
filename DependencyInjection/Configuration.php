891aa408d3e2c29e3d9479edfe672d362195165c
Alain Flaus <aflaus@prestaconcept.net>
2013-10-31 15:16:02 +0100

    Remove unused variable and unused file

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -23,11 +23,7 @@ class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('presta_cms_sitemap_bridge');
-
-        // Here you should define the parameters that are allowed to
-        // configure your bundle. See the documentation linked above for
-        // more information on that topic.
+        $treeBuilder->root('presta_cms_sitemap_bridge');
 
         return $treeBuilder;
     }
--- DependencyInjection/PrestaCMSSitemapBridgeExtension.php
+++ DependencyInjection/PrestaCMSSitemapBridgeExtension.php
@@ -32,7 +32,6 @@ class PrestaCMSSitemapBridgeExtension extends Extension
         $config = $this->processConfiguration($configuration, $configs);
 
         $loader = new Loader\XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
-        $loader->load('services.xml');
         $loader->load('listener.xml');
     }
 }
