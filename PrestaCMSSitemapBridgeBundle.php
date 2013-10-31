ea82538937c5fbbd2017089bb2f658c3dc6d5f40
Alain Flaus <aflaus@prestaconcept.net>
2013-10-31 14:29:07 +0100

    Forgotten bundle class

---
+++ PrestaCMSSitemapBridgeBundle.php
@@ -0,0 +1,17 @@
+<?php
+/**
+* This file is part of the PrestaCMSSitemapBridgeBundle
+ *
+ * (c) PrestaConcept <www.prestaconcept.net>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace Presta\CMSSitemapBridgeBundle;
+
+/**
+ * @author Alain Flaus <aflaus@prestaconcept.net>
+ */
+class PrestaCMSSitemapBridgeBundle
+{
+}
